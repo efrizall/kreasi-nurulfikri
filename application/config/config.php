3379f30d103bc3798b5069911a9bfc7392a1c95e
efrizall <rizal.ef340@gmail.com>
2021-12-17 16:48:53 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://kreasi.nurulfikri.ac.id/efrizal';
+$config['base_url'] = 'https://kreasi.nurulfikri.ac.id/efrizal';
 
 /*
 |--------------------------------------------------------------------------
